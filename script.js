83d7bd801fcce4f7ebfed8c4327c06f033cb008a
ST1LLWATER <62516824+ST1LLWATER@users.noreply.github.com>
2020-12-28 16:00:32 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,21 @@
+const toggleButton = document.getElementsByClassName("toggle-button")[0];
+const navbarContent = document.getElementsByClassName("navbar-content")[0];
+
+// const abc = classList;
+
+console.log(toggleButton);
+console.log(navbarContent);
+toggleButton.addEventListener("click", () => {
+  navbarContent.classList.toggle("active");
+  // document.getElementsById("nav").classList.add("active");0
+});
+
+// document
+//   .getElementsByClassName("toggle-button")
+//   .addEventListener("click", function () {
+//     var element = document.getElementsByClassName("navbar-content");
+//     element.classList.add("active");
+//   });
+
+const abc = document.getElementsById("nav");
+console.log(classList.abc);
